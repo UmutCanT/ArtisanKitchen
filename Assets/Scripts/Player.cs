5ce48c140169b0b0774184a0047f20a79efb029c
UmutCanT <uctekcambusiness@gmail.com>
2023-02-09 14:47:02 +0300

    Visual Change On Interaction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter selectedCounter;
+    }
+
     private const float INTERACT_DISTANCE = 2f;
 
     [SerializeField] private LayerMask countersLayerMask;
 
     private PlayerInput playerInput;
+    private ClearCounter selectedCounter;
     private Vector3 lastInteractDir;
 
     private void Awake()
@@ -23,20 +32,9 @@ public class Player : MonoBehaviour
 
     private void PlayerInput_OnInteractAction(object sender, System.EventArgs e)
     {
-        Vector2 inputVector = playerInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-
-        if (moveDir != Vector3.zero)
+        if(selectedCounter != null)
         {
-            lastInteractDir = moveDir;
-        }
-
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, INTERACT_DISTANCE, countersLayerMask))
-        {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
-            {
-                clearCounter.Interact();
-            }
+            selectedCounter.Interact();
         }
     }
 
@@ -59,8 +57,26 @@ public class Player : MonoBehaviour
         {
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
-                //clearCounter.Interact();
+                if (clearCounter != selectedCounter)
+                {
+                    ChangeSelectedCounter(clearCounter);                   
+                }
+            }
+            else
+            {
+                ChangeSelectedCounter(null);
             }
         }
+        else
+        {
+            ChangeSelectedCounter(null);
+        }
+    }
+
+    private void ChangeSelectedCounter(ClearCounter selectedCounter)
+    {
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs { selectedCounter = selectedCounter });
     }
 }
