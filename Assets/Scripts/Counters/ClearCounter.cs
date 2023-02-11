ae2b81144b7fc52430c51d009652fd3a76b07769
UmutCanT <uctekcambusiness@gmail.com>
2023-02-11 15:31:21 +0300

    Container Subclasses Simplified/Refactored

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -2,30 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : BaseCounter, ICanCarryKitchenObject
+public class ClearCounter : BaseCounter
 {
     [SerializeField] private KitchenObjectTemplete kitchenObjectTemplate;
-    [SerializeField] private Transform counterTopPoint;
-
-    private KitchenObject kitchenObject;
-    public KitchenObject KitchenObject { get => kitchenObject; set => kitchenObject = value; }
 
     public override void Interact(Player player)
     {
-        if (kitchenObject == null)
-        {
-            GameObject spawnedKitchenObject = Instantiate(kitchenObjectTemplate.Prefab, counterTopPoint);
-            spawnedKitchenObject.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
-        }
-        else
-        {
-            kitchenObject.SetKitchenObjectParent(player);
-        }       
+        
     }
-
-    public Transform GetParentTransform() => counterTopPoint;
-
-    public void ClearKitchenObject() => kitchenObject = null;
-
-    public bool HasKitchenObject() => kitchenObject != null;
 }
--- Assets/Scripts/Counters/ContainerCounter.cs
+++ Assets/Scripts/Counters/ContainerCounter.cs
@@ -2,31 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ContainerCounter : BaseCounter, ICanCarryKitchenObject
+public class ContainerCounter : BaseCounter
 {
     [SerializeField] private KitchenObjectTemplete kitchenObjectTemplate;
-    [SerializeField] private Transform counterTopPoint;
-
-    private KitchenObject kitchenObject;
-
-    public KitchenObject KitchenObject { get => kitchenObject; set => kitchenObject = value; }
 
     public override void Interact(Player player)
     {
-        if (kitchenObject == null)
-        {
-            GameObject spawnedKitchenObject = Instantiate(kitchenObjectTemplate.Prefab, counterTopPoint);
-            spawnedKitchenObject.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
-        }
-        else
-        {
-            kitchenObject.SetKitchenObjectParent(player);
-        }
+        GameObject spawnedKitchenObject = Instantiate(kitchenObjectTemplate.Prefab);
+        spawnedKitchenObject.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
     }
-
-    public Transform GetParentTransform() => counterTopPoint;
-
-    public void ClearKitchenObject() => kitchenObject = null;
-
-    public bool HasKitchenObject() => kitchenObject != null;
 }
