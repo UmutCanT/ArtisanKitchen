0d31f5b156de0c896bf9dcee9255c28bf1f55c4b
UmutCanT <uctekcambusiness@gmail.com>
2023-02-11 15:41:51 +0300

    Container Counter Animation

--- Assets/Scripts/Counters/ContainerCounter.cs
+++ Assets/Scripts/Counters/ContainerCounter.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ContainerCounter : BaseCounter
 {
+    public event EventHandler OnPlayerOpenContainer;
+
     [SerializeField] private KitchenObjectTemplete kitchenObjectTemplate;
 
     public override void Interact(Player player)
     {
         GameObject spawnedKitchenObject = Instantiate(kitchenObjectTemplate.Prefab);
         spawnedKitchenObject.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+        OnPlayerOpenContainer?.Invoke(this, EventArgs.Empty);
     }
 }
