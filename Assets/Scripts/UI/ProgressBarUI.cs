988fa237cea48d33271c63ea3110799ac6bbd8ec
UmutCanT <uctekcambusiness@gmail.com>
2023-02-13 12:58:21 +0300

    Bar Visibility According to Interaction

--- Assets/Scripts/UI/ProgressBarUI.cs
+++ Assets/Scripts/UI/ProgressBarUI.cs
@@ -12,10 +12,24 @@ public class ProgressBarUI : MonoBehaviour
     {
         cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
         barImage.fillAmount = 0f;
+        ShowProgressBar(false);
     }
 
     private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e)
     {
         barImage.fillAmount = e.progressNormalized;
+        if (e.progressNormalized == 0f || e.progressNormalized == 1f)
+        {
+            ShowProgressBar(false);
+        }
+        else
+        {
+            ShowProgressBar(true);
+        }
+    }
+
+    private void ShowProgressBar(bool isActive)
+    {
+        gameObject.SetActive(isActive);
     }
 }
