eeccb71de46ac11f7f1d1d28530a2c6c2d290c4a
UmutCanT <uctekcambusiness@gmail.com>
2023-02-13 17:20:20 +0300

    World Canvas UI's Look at Modes

---
+++ Assets/Scripts/UI/LookAtCamera.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    [SerializeField] private LookAtMode lookAtMode;
+
+    private void LateUpdate()
+    {
+        switch (lookAtMode)
+        {
+            case LookAtMode.Normal:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case LookAtMode.Inverted:
+                Vector3 dirFromCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + dirFromCamera);
+                break;
+            case LookAtMode.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            case LookAtMode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private enum LookAtMode
+    {
+        Normal,
+        Inverted,
+        CameraForward,
+        CameraForwardInverted
+    }
+}
