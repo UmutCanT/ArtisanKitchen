d6fd6c6e39097b7e0924b9fbde1cc5a07b19c555
UmutCanT <uctekcambusiness@gmail.com>
2023-02-14 09:10:08 +0300

    Frying Coroutine and States

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -5,21 +5,53 @@ using UnityEngine;
 public class StoveCounter : BaseCounter
 {
     [SerializeField] private FryingRecipe[] fryingRecipes;
+
+    private State currentState;
     private FryingRecipe fryingRecipe;
     private float fryingTimer;
 
-    private void Update()
+    private void Start()
     {
-        if (HasKitchenObject())
+        currentState = State.None;
+    }
+
+    private IEnumerator Fry()
+    {
+        fryingRecipe = GetFryingRecipeWithInputObj(KitchenObj.ObjectTemplate);
+        fryingTimer = 0;
+        switch (currentState)
         {
-            fryingTimer += Time.deltaTime;
-            if (fryingTimer >= fryingRecipe.TotalFryingTime)
-            {
-                fryingTimer = 0;
+            case State.None:
+                currentState = State.Frying;               
+                StartCoroutine(Fry());
+                break;
+            case State.Frying:
+                while (fryingTimer <= fryingRecipe.TotalFryingTime)
+                {
+                    fryingTimer += Time.deltaTime;
+                    yield return null;
+                }
                 KitchenObj.RemovingItself();
                 KitchenObject.SpawnKitchenObject(fryingRecipe.FryedObj, this);
-            }
+                currentState= State.Fried;
+                StartCoroutine(Fry());
+                break;
+            case State.Fried:
+                while (fryingTimer <= fryingRecipe.TotalFryingTime)
+                {
+                    fryingTimer += Time.deltaTime;
+                    yield return null;
+                }
+                KitchenObj.RemovingItself();
+                KitchenObject.SpawnKitchenObject(fryingRecipe.FryedObj, this);
+                currentState = State.Burned;
+                break;
+            case State.Burned:
+                break;
+            default:
+                break;
         }
+        
     }
 
     public override void Interact(Player player)
@@ -30,8 +62,8 @@ public class StoveCounter : BaseCounter
             {
                 if (InputObjectHasRecipe(player.KitchenObj.ObjectTemplate))
                 {
-                    player.KitchenObj.SetKitchenObjectParent(this); 
-                    fryingRecipe = GetFryingRecipeWithInputObj(KitchenObj.ObjectTemplate);
+                    player.KitchenObj.SetKitchenObjectParent(this);                                     
+                    StartCoroutine(Fry());
                 }
             }
         }
@@ -44,10 +76,18 @@ public class StoveCounter : BaseCounter
             else
             {
                 KitchenObj.SetKitchenObjectParent(player);
+                StopAllCoroutines();
+                currentState = StateDecider();
             }
         }
     }
 
+    private State StateDecider()
+    {
+        if (currentState == State.Fried)
+            return State.Fried;
+        return State.None;
+    }
 
     private bool InputObjectHasRecipe(KitchenObjectTemplate inputObj)
     {
@@ -78,5 +118,13 @@ public class StoveCounter : BaseCounter
             }
         }
         return null;
+    }
+
+    private enum State
+    {
+        None,
+        Frying,
+        Fried,
+        Burned
     }
 }
