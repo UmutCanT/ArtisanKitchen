a9a4d09494b0d5ebe4db057906aa04ef0e40e825
UmutCanT <uctekcambusiness@gmail.com>
2023-02-14 11:41:55 +0300

    Progress Bar for Stove Counter

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -3,9 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoveCounter : BaseCounter
+public class StoveCounter : BaseCounter, ICanProgress
 {
     public event EventHandler<OnStateChangeEventArgs> OnStateChange;
+    public event EventHandler<ICanProgress.OnProgressChangedEventArgs> OnProgressChanged;
 
     public class OnStateChangeEventArgs : EventArgs
     {
@@ -23,58 +24,6 @@ public class StoveCounter : BaseCounter
     private void Start()
     {
         currentState = State.None;
-    }
-
-    private IEnumerator Fry()
-    {
-        fryingRecipe = GetFryingRecipeWithInputObj(KitchenObj.ObjectTemplate);
-        fryingTimer = 0;
-
-        switch (currentState)
-        {
-            case State.None:
-                currentState = State.Frying;
-                OnStateChange?.Invoke(this, new OnStateChangeEventArgs
-                {
-                    state = currentState
-                });
-                StartCoroutine(Fry());
-                break;
-            case State.Frying:
-                while (fryingTimer <= fryingRecipe.TotalFryingTime)
-                {
-                    fryingTimer += Time.deltaTime;
-                    yield return null;
-                }
-                KitchenObj.RemovingItself();
-                KitchenObject.SpawnKitchenObject(fryingRecipe.FryedObj, this);
-                currentState= State.Fried;
-                OnStateChange?.Invoke(this, new OnStateChangeEventArgs 
-                { 
-                    state = currentState 
-                });
-                StartCoroutine(Fry());
-                break;
-            case State.Fried:
-                overcookedRecipe = GetOvercookedRecipeWithInputObj(KitchenObj.ObjectTemplate);
-                while (fryingTimer <= overcookedRecipe.TotalOvercookTime)
-                {
-                    fryingTimer += Time.deltaTime;
-                    yield return null;
-                }
-                KitchenObj.RemovingItself();
-                KitchenObject.SpawnKitchenObject(overcookedRecipe.BurnedObj, this);
-                currentState = State.Burned;
-                OnStateChange?.Invoke(this, new OnStateChangeEventArgs 
-                { 
-                    state = currentState 
-                });
-                break;
-            case State.Burned:
-                break;
-            default:
-                break;
-        }     
     }
 
     public override void Interact(Player player)
@@ -101,11 +50,78 @@ public class StoveCounter : BaseCounter
                 KitchenObj.SetKitchenObjectParent(player);
                 StopAllCoroutines();
                 currentState = State.None;
+                OnProgressChanged?.Invoke(this, new ICanProgress.OnProgressChangedEventArgs
+                {
+                    progressNormalized = 0f
+                });
                 OnStateChange?.Invoke(this, new OnStateChangeEventArgs
                 {
                     state = currentState
                 });
             }
+        }
+    }
+
+    private IEnumerator Fry()
+    {
+        fryingRecipe = GetFryingRecipeWithInputObj(KitchenObj.ObjectTemplate);
+        fryingTimer = 0;
+        switch (currentState)
+        {
+            case State.None:
+                currentState = State.Frying;
+                OnStateChange?.Invoke(this, new OnStateChangeEventArgs
+                {
+                    state = currentState
+                });
+                StartCoroutine(Fry());
+                break;
+            case State.Frying:
+                while (fryingTimer <= fryingRecipe.TotalFryingTime)
+                {
+                    fryingTimer += Time.deltaTime;
+                    OnProgressChanged?.Invoke(this, new ICanProgress.OnProgressChangedEventArgs
+                    {
+                        progressNormalized = (float)fryingTimer / fryingRecipe.TotalFryingTime
+                    });
+                    yield return null;
+                }
+                KitchenObj.RemovingItself();
+                KitchenObject.SpawnKitchenObject(fryingRecipe.FryedObj, this);
+                currentState = State.Fried;
+                OnStateChange?.Invoke(this, new OnStateChangeEventArgs
+                {
+                    state = currentState
+                });
+                StartCoroutine(Fry());
+                break;
+            case State.Fried:
+                overcookedRecipe = GetOvercookedRecipeWithInputObj(KitchenObj.ObjectTemplate);
+                while (fryingTimer <= overcookedRecipe.TotalOvercookTime)
+                {
+                    fryingTimer += Time.deltaTime;
+                    OnProgressChanged?.Invoke(this, new ICanProgress.OnProgressChangedEventArgs
+                    {
+                        progressNormalized = (float)fryingTimer / overcookedRecipe.TotalOvercookTime
+                    });
+                    yield return null;
+                }
+                KitchenObj.RemovingItself();
+                KitchenObject.SpawnKitchenObject(overcookedRecipe.BurnedObj, this);
+                currentState = State.Burned;
+                OnStateChange?.Invoke(this, new OnStateChangeEventArgs
+                {
+                    state = currentState
+                });
+                OnProgressChanged?.Invoke(this, new ICanProgress.OnProgressChangedEventArgs
+                {
+                    progressNormalized = 0f
+                });
+                break;
+            case State.Burned:
+                break;
+            default:
+                break;
         }
     }
 
