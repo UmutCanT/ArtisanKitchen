87cc86b013f7ad70d2c93e715344a858d11dc2b3
UmutCanT <uctekcambusiness@gmail.com>
2023-02-17 09:10:59 +0300

    Playing Sound Effects From Manager

--- Assets/Scripts/Counters/CuttingCounter.cs
+++ Assets/Scripts/Counters/CuttingCounter.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class CuttingCounter : BaseCounter, ICanProgress
 {
+    public static event EventHandler OnAnyCut;
     public event EventHandler<ICanProgress.OnProgressChangedEventArgs> OnProgressChanged;
     public event EventHandler OnCut;
 
@@ -56,6 +57,7 @@ public class CuttingCounter : BaseCounter, ICanProgress
             CuttingRecipe cuttingRecipe = GetCuttingRecipeWithRawObj(KitchenObj.ObjectTemplate);
 
             OnCut?.Invoke(this, EventArgs.Empty);
+            OnAnyCut?.Invoke(this, EventArgs.Empty);
             OnProgressChanged?.Invoke(this, new ICanProgress.OnProgressChangedEventArgs
             {
                 progressNormalized = (float)cuttingProgress / cuttingRecipe.NumberOfStepsToProcess
--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -1,14 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TrashCounter : BaseCounter
 {
+    public static event EventHandler OnAnyObjectThrashed;
+
     public override void Interact(Player player)
     {
         if (player.HasKitchenObject())
         {
             player.KitchenObj.RemovingItself();
+
+            OnAnyObjectThrashed?.Invoke(this, EventArgs.Empty);
         }
     }
 }
