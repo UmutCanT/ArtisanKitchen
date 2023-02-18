723df229882f02228f36bb068fd313b6962c26b8
UmutCanT <uctekcambusiness@gmail.com>
2023-02-18 15:23:54 +0300

    Basic Loading Scene Implemented

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,9 +13,15 @@ public static class SceneLoader
         LoadingScene
     }
 
-    public static int targetSceneIndex;
+    private static Scene targetScene;
 
     public static void Load(Scene targetScene)
+    {
+        SceneLoader.targetScene = targetScene;
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+    }
+
+    public static void LoaderCallback()
     {
         SceneManager.LoadScene(targetScene.ToString());
     }
