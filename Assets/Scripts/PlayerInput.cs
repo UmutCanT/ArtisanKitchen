baa21807183e2277264ea00e96b64b6e0ef5b891
UmutCanT <uctekcambusiness@gmail.com>
2023-02-18 17:21:40 +0300

    Fixing Restarting Game Bugs

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -20,6 +20,15 @@ public class PlayerInput : MonoBehaviour, IMovementInput
         playerInputActions.Player.Pause.performed += Pause_performed;
     }
 
+    private void OnDestroy()
+    {
+        playerInputActions.Player.Interact.performed -= Interact_performed;
+        playerInputActions.Player.InteractAlternate.performed -= InteractAlternate_performed;
+        playerInputActions.Player.Pause.performed -= Pause_performed;
+
+        playerInputActions.Dispose();
+    }
+
     private void Pause_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         //OnPauseAction?.Invoke(this, EventArgs.Empty);
